beb81d05514faa6e5e4511f2d5e61373cca9ea2a
nift4 <nift4@protonmail.com>
2023-04-14 00:37:39 +0200

    vendor: Remove useless addon.d file
    * Avoid detection as custom ROM
    
    Change-Id: I1fbd63cc116d53c7c366662b632163f6bc079bbf
    Signed-off-by: nadinsylaa <asepmlnaaa@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -30,11 +30,7 @@ endif
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/nava/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
-    vendor/nava/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
-    vendor/nava/prebuilt/common/bin/50-lineage.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/50-lineage.sh
-
-PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
-    system/addon.d/50-nava.sh
+    vendor/nava/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions
 
 ifneq ($(strip $(AB_OTA_PARTITIONS) $(AB_OTA_POSTINSTALL_CONFIG)),)
 PRODUCT_COPY_FILES += \
